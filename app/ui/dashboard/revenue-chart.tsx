2beb7cebfa3d4d288b98b4fe299e7db6c194034e
nameRoy <47552262+nameRoy@users.noreply.github.com>
2024-01-05 15:48:56 +0800

    Chapter 12 finish

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,12 +1,9 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+import { fetchRevenue } from '@/app/lib/data';
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue(); // Fetch data inside the component
   const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
